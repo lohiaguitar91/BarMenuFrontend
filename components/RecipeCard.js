3828de5896b4a50a6df452d4b99709d3c803b9b4
lohiaguitar91 <kaushik@lohia.net>
2023-04-02 19:16:06 -0700

    Add recipe image, steps, and improve styling.
    This commit adds the recipe image to the list view with a forced size.
    It also adds the IndividualRecipeCard, which is what the individual
    drink/recipe view uses. This will allow for better separation, for
    example showing steps and an "order" button for the individual view.
    
    Included some stylistic changes to CSS, including spacing between
    ingredients and steps for the individual recipe view - but it is a bit
    too spaced.

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,14 +1,14 @@
 import { useRouter } from 'next/router'; // Using instead of Link because problems that probably don't exist, could change back later...
 import Link from 'next/link'
 
-const RecipeCard = ({ recipe, onViewClick }) => {
+const RecipeCard = ({ recipe }) => {
   const router = useRouter();
   return (
     <div className="card recipe-card">
       <div className="card-body">
-        <h5 className="card-title">{recipe.name}</h5>
-        <p className="card-text">
-          <strong>Ingredients:</strong> {recipe.ingredients}
+        <h3 className="card-title">{recipe.name}</h3>
+        <p className="card-image">
+          <img src={recipe.picture} width="200" height="200"></img>
         </p>
         <div className="action-buttons">
         <button className="btn btn-sm btn-success">
@@ -25,6 +25,7 @@ const RecipeCard = ({ recipe, onViewClick }) => {
       <style jsx>{`
         .recipe-card {
           box-shadow: 0 1rem 1.5rem rgba(0, 0, 0, 0.6);
+          display: inline-block;
         }
       `}</style>
     </div>
